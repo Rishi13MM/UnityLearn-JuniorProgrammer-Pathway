65b23c1f817c2ab32f016242c2d1b7dbf66ddb73
Rishi <rishimunijnp1@gmail.com>
2022-10-31 02:06:15 +0530

    Fix 6 - Bonus: The spawn interval is always the same

--- Mission - Create With Code 1/Unit 2 - Basic Gameplay/Challenge 2 - Play Fetch/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Mission - Create With Code 1/Unit 2 - Basic Gameplay/Challenge 2 - Play Fetch/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -11,23 +11,37 @@ public class SpawnManagerX : MonoBehaviour
     private float spawnPosY = 30;
 
     private float startDelay = 1.0f;
-    private float spawnInterval = 4.0f;
+    private float spawnInterval;
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+        StartCoroutine(SpawnRandomBallRoutine());
     }
 
     // Spawn random ball at random x position at top of play area
-    void SpawnRandomBall ()
+    void SpawnRandomBall()
     {
         // Generate random ball index and random spawn position
-        int ballIndex = Random.Range(0,ballPrefabs.Length);
+        int ballIndex = Random.Range(0, ballPrefabs.Length);
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 
         // instantiate ball at random spawn location
         Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
+    }
+
+    IEnumerator SpawnRandomBallRoutine()
+    {
+        yield return new WaitForSeconds(startDelay);
+        SpawnRandomBall();
+
+        while (true)
+        {
+            // Generate random spawnInterval b/w 3 and 5
+            spawnInterval = Random.Range(3f, 5f);
+            yield return new WaitForSeconds(spawnInterval);
+            SpawnRandomBall();
+        }
     }
 
 }
