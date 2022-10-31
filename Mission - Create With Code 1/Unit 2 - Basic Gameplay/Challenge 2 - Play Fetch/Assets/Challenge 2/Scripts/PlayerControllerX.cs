d569764ae56ed837a121d02ad8ea25da2701af7b
Rishi <rishimunijnp1@gmail.com>
2022-10-31 08:15:30 +0530

    Fix 7 - Bonus: The player can “spam” the spacebar key

--- Mission - Create With Code 1/Unit 2 - Basic Gameplay/Challenge 2 - Play Fetch/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Mission - Create With Code 1/Unit 2 - Basic Gameplay/Challenge 2 - Play Fetch/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,14 +5,24 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    private bool isDogReady = true;
+    private float spawnDelay = 1f;
 
     // Update is called once per frame
     void Update()
     {
-        // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        // On spacebar press and isDogReady is true, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && isDogReady)
         {
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            StartCoroutine(isSpawnActiveRoutine());
         }
+    }
+
+    IEnumerator isSpawnActiveRoutine()
+    {
+        isDogReady = false;
+        yield return new WaitForSeconds(spawnDelay);
+        isDogReady = true;
     }
 }
