aae1f185a9c26705aebba3836f803954e41e3821
Rishi <rishimunijnp1@gmail.com>
2022-11-23 13:51:38 +0530

    Mod the Cube - Exercise completed

--- Mission - Create With Code 1/Exercise/Mod the Cube/Assets/ModTheCube/Cube.cs
+++ Mission - Create With Code 1/Exercise/Mod the Cube/Assets/ModTheCube/Cube.cs
@@ -5,19 +5,32 @@ using UnityEngine;
 public class Cube : MonoBehaviour
 {
     public MeshRenderer Renderer;
+    private float rotateSpeed;
+    private float cubeSize;
     
     void Start()
     {
+        InvokeRepeating("ChangeCubeColor",1.0f,3f);
+        rotateSpeed = Random.Range(10f,30f);
+        cubeSize = Random.Range(1f,5f);
+
         transform.position = new Vector3(3, 4, 1);
-        transform.localScale = Vector3.one * 1.3f;
         
         Material material = Renderer.material;
         
         material.color = new Color(0.5f, 1.0f, 0.3f, 0.4f);
+        transform.localScale = Vector3.one * cubeSize;
+
     }
     
     void Update()
     {
-        transform.Rotate(10.0f * Time.deltaTime, 0.0f, 0.0f);
+        transform.Rotate(rotateSpeed * Time.deltaTime, 0.0f, 0.0f);
+    }
+
+    void ChangeCubeColor()
+    {
+        Color cubeColor = new Color(Random.Range(0f,255f)/255f,Random.Range(0f,255f)/255f,Random.Range(0f,255f)/255f,Random.Range(0f,1f));
+        Renderer.material.color = cubeColor;
     }
 }
